31f7523ee18547ce1bc645417bcbad4316c02f51
vinitpatil2812 <vinitpatil280@gmail.com>
2024-03-01 16:55:57 +0530

    socket signalling setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const { Server, Socket } = require("socket.io");
 
-const io = new Server(8000);
+const io = new Server(8000, {
+    cors: true,
+});
+
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
 
 io.on('connection', (socket) => {
+    socket.on("room:join", (data) => {
+        const { email, room } = data;
 
+        emailToSocketIdMap.set(email, socket.id);
+        socketIdToEmailMap.set(socket.id, email);
+
+        io.to(socket.id).emit("room:join", data);
+    });
 });

deecf1f17842008af31919021a61d359d00cc0f5
vinitpatil2812 <vinitpatil280@gmail.com>
2024-03-02 09:53:54 +0530

    Room setup

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ io.on('connection', (socket) => {
         emailToSocketIdMap.set(email, socket.id);
         socketIdToEmailMap.set(socket.id, email);
 
-        io.to(socket.id).emit("room:join", data);
+        io.to(room).emit("user:joined", {email, id: socket.id });
+        socket.join(room);
+
+        io.to(socket.id).emit("room:join", {email, room});
     });
 });
